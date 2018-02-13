7ef5e74cc666df3daa9f26dd372e90df35f88744
novakvova <novakvova@gmail.com>
2018-02-13 18:48:09 +0200

    Add image product save to memory

--- BLL/Model/Photo.cs
+++ BLL/Model/Photo.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace BLL.Model
@@ -20,40 +22,35 @@ namespace BLL.Model
 
         public Photo() { }
         public Photo(string path)
-        {
-            if(!(Directory.Exists(Environment.CurrentDirectory + ConfigurationManager.AppSettings["ImagePath"].ToString())))
-                    Directory.CreateDirectory(Environment.CurrentDirectory + ConfigurationManager.AppSettings["ImagePath"].ToString());
-            // 
+        { 
             _pathOriginal = path;
-            //_path = "L:\\Temp\\" + Guid.NewGuid().ToString() + ".jpg";
-            _path = Environment.CurrentDirectory + ConfigurationManager.AppSettings["ImagePath"].ToString() + Guid.NewGuid().ToString() + ".jpg";
-            using (var image = System.Drawing.Image.FromFile(_pathOriginal))
+           using (var image = System.Drawing.Image.FromFile(_pathOriginal))
             {
                 var newImageSmall = ImageWorker.ConverImageToBitmap(image, 130, 130);
                 if (newImageSmall != null)
                 {
-                    newImageSmall.Save(_path, System.Drawing.Imaging.ImageFormat.Jpeg);
-                    _image = BitmapFrame.Create(new Uri(_path));
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        newImageSmall.Save(ms,ImageFormat.Bmp);
+                        _image = BitmapFrame.Create(ms, 
+                            BitmapCreateOptions.PreservePixelFormat, 
+                            BitmapCacheOption.OnLoad);
+                    }
                 }
-                //_source = new Uri(path);
             }
         }
-
+        
         public Photo(ProductImage path) // конструктор получения изображений из базы
         {
             _path = path.NameSmall;
             _pathOriginal = path.NameOriginal;
         }
-
-        //public override string ToString()
-        //{
-        //    return _source.ToString();
-        //}
+        
         public string Source { get { return _path; } } // путь на уменьшенную фотку
 
         public string SourceOriginal { get { return _pathOriginal; } } // путь на оригинальную фотку
 
-        public BitmapFrame Image { get { return _image; } set { _image = value; } }
+        public BitmapFrame ImageFrame { get { return _image; } set { _image = value; } }
 
         public string ImageName { get { return Path.GetFileNameWithoutExtension(_pathOriginal); } }
 
